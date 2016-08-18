959fdb9e85ab68cc8c032366ad79723f35ca3951
Navneet-Bedi <bedinavneet91@gmail.com>
2016-08-18 12:38:39 -0700

    Update ConvManagedBean.java

--- src/ConvManagedBean.java
+++ src/ConvManagedBean.java
@@ -23,7 +23,7 @@ import org.w3c.dom.Element;
 
 /**
  *
- * @author NAVNEET
+ * @author Harshitha
  */
 @ManagedBean
 @SessionScoped
@@ -32,82 +32,77 @@ public class ConvManagedBean {
     /**
      * Creates a new instance of ConversionBean
      */
-    
-    
-   String datafield="";
-   String ftomtr="Feet to Meter";
-   String gtoltr="Gallon to Liter";
-   String convradio="";
-  
+    String datafield = "";
+    String ftomtr = "Feet to Meter";
+    String gtoltr = "Gallon to Liter";
+    String convradio = "";
 
-   
-    public void setDatafield(String s)
-    {
-        datafield=s;
+    public void setDatafield(String s) {
+        datafield = s;
     }
-    public String getDatafield()
-    {
-       return datafield;
+
+    public String getDatafield() {
+        return datafield;
     }
-    public void setConvradio(String s)
-    {
-        convradio=s;
+
+    public void setConvradio(String s) {
+        convradio = s;
     }
-    public String getConvradio()
-    {
+
+    public String getConvradio() {
         return convradio;
     }
-      public String send() {
-          String str="";
-    try {
-    // Create connection, message factory, and SOAP factory
-       SOAPConnectionFactory soapConnectionFactory =
-                SOAPConnectionFactory.newInstance();
-       SOAPConnection connection =
-                soapConnectionFactory.createConnection();
-       MessageFactory messageFactory = MessageFactory.newInstance();
-       SOAPFactory soapFactory = SOAPFactory.newInstance();
 
-    // Create a message
-       SOAPMessage message = messageFactory.createMessage();
+    public String send() {
+        String str = "";
+        try {
+            // Create connection, message factory, and SOAP factory
+            SOAPConnectionFactory soapConnectionFactory
+                    = SOAPConnectionFactory.newInstance();
+            SOAPConnection connection
+                    = soapConnectionFactory.createConnection();
+            MessageFactory messageFactory = MessageFactory.newInstance();
+            SOAPFactory soapFactory = SOAPFactory.newInstance();
 
-       SOAPPart soapPart = message.getSOAPPart();
-       SOAPEnvelope envelope = soapPart.getEnvelope();
-       envelope.addNamespaceDeclaration("enc","http://schemas.xmlsoap.org/soap/encoding/");
-       Name encStyle = soapFactory.createName("encodingStyle","SOAP-ENV","http://schemas.xmlsoap.org/soap/envelope/");
-       envelope.addAttribute(encStyle,"http://schemas.xmlsoap.org/soap/encoding/");
+            // Create a message
+            SOAPMessage message = messageFactory.createMessage();
 
-    // Get the SOAP header from the message and remove it
-       SOAPHeader header = message.getSOAPHeader();
-       header.detachNode();
+            SOAPPart soapPart = message.getSOAPPart();
+            SOAPEnvelope envelope = soapPart.getEnvelope();
+            envelope.addNamespaceDeclaration("enc", "http://schemas.xmlsoap.org/soap/encoding/");
+            Name encStyle = soapFactory.createName("encodingStyle", "SOAP-ENV", "http://schemas.xmlsoap.org/soap/envelope/");
+            envelope.addAttribute(encStyle, "http://schemas.xmlsoap.org/soap/encoding/");
 
-    // Get the SOAP body from the message
-       SOAPBody body = message.getSOAPBody();
+            // Get the SOAP header from the message and remove it
+            SOAPHeader header = message.getSOAPHeader();
+            header.detachNode();
 
-       Name bodyName = soapFactory.createName("convert","tns","http://bc9959/");
-       SOAPBodyElement bodyElement = body.addBodyElement(bodyName);
-       Name nm = soapFactory.createName("arg0");
-       SOAPElement arg0 = bodyElement.addChildElement(nm);
-              arg0.addTextNode(datafield);
-       Name nm1 = soapFactory.createName("arg1");
-       SOAPElement arg1 = bodyElement.addChildElement(nm1);
-       arg1.addTextNode(convradio);
+            // Get the SOAP body from the message
+            SOAPBody body = message.getSOAPBody();
 
-       message.saveChanges();
-   
-       URL endpoint = new URL("http://134.154.10.165:8080/bc9959ServiceBeanService/bc9959ServiceBean?");
+            Name bodyName = soapFactory.createName("convert", "tns", "http://bc9959/");
+            SOAPBodyElement bodyElement = body.addBodyElement(bodyName);
+            Name nm = soapFactory.createName("arg0");
+            SOAPElement arg0 = bodyElement.addChildElement(nm);
+            arg0.addTextNode(datafield);
+            Name nm1 = soapFactory.createName("arg1");
+            SOAPElement arg1 = bodyElement.addChildElement(nm1);
+            arg1.addTextNode(convradio);
 
-       SOAPMessage reply = connection.call(message, endpoint);
-       org.w3c.dom.NodeList nl=reply.getSOAPBody().getElementsByTagName("return");
-       Element result = (Element) nl.item(0);
-       str = result.getFirstChild().getNodeValue();
+            message.saveChanges();
 
-    
-    // Close the connection
-       connection.close();
+            URL endpoint = new URL("http://134.154.10.165:8080/bc9959ServiceBeanService/bc9959ServiceBean?");
+
+            SOAPMessage reply = connection.call(message, endpoint);
+            org.w3c.dom.NodeList nl = reply.getSOAPBody().getElementsByTagName("return");
+            Element result = (Element) nl.item(0);
+            str = result.getFirstChild().getNodeValue();
+
+            // Close the connection
+            connection.close();
         } catch (Exception ex) {
-         //            ex.printStackTrace();
+            //            ex.printStackTrace();
         }
-       return str;
+        return str;
     }
 }
